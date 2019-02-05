08ee13df52e86b4d764b4a7819c8fe04df51a96e
Franz B <sneils@gmx.at>
2019-02-05 13:15:38 +0100

    Make sure db port is always an interger

--- agent.py
+++ agent.py
@@ -37,7 +37,7 @@ def main():
     if not "connection" in config:
         config["connection"] = {
             "host": os.environ["DB_HOST"],
-            "port": os.environ["DB_PORT"],
+            "port": int(os.environ["DB_PORT"]),
             "user": os.environ["DB_USER"],
             "password": os.environ["DB_PASSWORD"],
             "db": os.environ["DB_NAME"],
